83b6634fe2024d6fd6c63a50df79aa2930ade325
Joe <jyup216@gmail.com>
2026-01-08 14:38:28 -0500

    removed some comments

--- AI_Voice_Assistant.py
+++ AI_Voice_Assistant.py
@@ -14,17 +14,14 @@ import threading
 ctk.set_appearance_mode("system") # can support system/dark/light mode
 ctk.set_default_color_theme("blue") # colors are kinda limited
 
-# inheriting from customTKinter as ctk
 class AIAssistant(ctk.CTk):
     def __init__(self):
         super().__init__()
         
-        # standard framework 
         self.title("AI Speech-to-Text Assistant")
         self.geometry("720x720")
         self.iconbitmap("logo_icon.ico")
 
-        # define label for mic selection
         self.miclabel = ctk.CTkLabel(self, text="Select Microphone:", font = ("Arial", 14, "bold"))
         self.miclabel.pack(pady=(20,5))
 
@@ -32,22 +29,18 @@ class AIAssistant(ctk.CTk):
         self.micDropdown = ctk.CTkOptionMenu(self, values = self.micOptions)
         self.micDropdown.pack(pady=10)
 
-        # label to tell us status
         self.statusLabel = ctk.CTkLabel(self, text="Ready...", text_color="gray")
         self.statusLabel.pack(pady=5)
         
-        # button to start listening
         self.button = ctk.CTkButton(self, text = "Start Listening", font = ("Arial", 14, "bold"), height = 40, command=self.startThread)
         self.button.pack(pady=20)
 
-        # box to display our output
         self.outputLabel = ctk.CTkLabel(self, text = "Output:", anchor= "w")
         self.outputLabel.pack(pady =(10, 0), padx=20, anchor = "w")
 
         self.textbox = ctk.CTkTextbox(self, font=("Consolas", 14))
         self.textbox.pack(pady=10, padx = 20, fill = "both", expand = True)
 
-    # this helper uses SpeechRecognition library to show available on mics and find their index
     def getMicrophoneList(self):
         try:
             mics = sr.Microphone.list_microphone_names()
@@ -55,14 +48,12 @@ class AIAssistant(ctk.CTk):
         except:
             return ["0: Default Microphone"]
 
-    # have a thread to run GUI + run in background
     def startThread(self):
         self.button.configure(state = "disabled", text = "Running...")
         self.statusLabel.configure(text="Initializing...", text_color="#3B8ED0")
 
         threading.Thread(target=self.logic).start()
-
-    # we want 5 steps: (1) CHOOSE MIC (2) LISTEN (3) TRANSCRIBE (4) SEND TO OLLAMA (5) REPLY
+        
     def logic(self):
 
         # choose our mic
@@ -75,22 +66,19 @@ class AIAssistant(ctk.CTk):
             index = int(mic.split(":")[0])
 
             with sr.Microphone(device_index=index) as source:
-
-                # start listening
+                
                 self.updateStatus("Adjusting...", "orange")
                 recognizer.adjust_for_ambient_noise(source, duration = 1)
 
                 self.updateStatus("Listening...", "green")
                 audio = recognizer.listen(source, timeout=10, phrase_time_limit=30)
 
-                # transcribe
                 self.updateStatus("Processing...", "orange")
                 transcribe = recognizer.recognize_google(audio)
 
                 self.textbox.insert("end", f"YOU: {transcribe}\n")
                 self.textbox.see("end")
 
-                # reply
                 self.updateStatus("Thinking...", "blue")
 
                 response = ollama.chat(model = 'llama3', messages = [{'role': 'user', 'content': transcribe}])
@@ -108,7 +96,6 @@ class AIAssistant(ctk.CTk):
         finally:
             self.button.configure(state = "normal", text = "Start Listening")
 
-    # another helper function to edit text/colors
     def updateStatus(self, text, color):
         self.statusLabel.configure(text=text, text_color=color)
 
